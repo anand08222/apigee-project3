d56111da48def86b428f86c9305433182bacf0fe
Anand Tiwari <64299402+anand08222@users.noreply.github.com>
2020-05-04 14:10:00 +0530

    Update aapTest.js

--- test/aapTest.js
+++ test/aapTest.js
@@ -1,5 +1,5 @@
-const assert = require('chai').assert;
-const app = require('../app');
+var app = require('./add.js');
+var assert = require('chai').assert
 
 describe('App', function(){
 	it('app should return hello',function(){
